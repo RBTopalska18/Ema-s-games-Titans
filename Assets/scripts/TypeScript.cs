bd6daabb1fecf11c5bb658e2a8e93c06c93806f7
kddinev18 <kddinev18@codingburgas.bg>
2021-06-26 18:28:35 +0300

    Implement TypeScript and WordGame scripts

---
+++ Assets/scripts/TypeScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Linq;
+using Random = UnityEngine.Random;
+
+public class TypeScript : MonoBehaviour
+{
+    public Text wordOutput;
+    public Text scoreText;
+    private string container;
+    public InputField inputfieldname;
+
+    private string[] bg = {"здравей","довиждане","ябълка","морков"};
+    private string[] en = { "Hello", "Bye", "Apple", "Carrot"};
+    private int randomIndex;
+    private int score = 0;
+
+    private void Start()
+    {
+        scoreText.text = score.ToString();
+        randomIndex = Random.Range(0, bg.Length);
+        wordOutput.text = en[randomIndex];
+        getAnswer();
+    }
+
+    void Update()
+    {
+        if(container == bg[randomIndex] && Input.GetKeyDown(KeyCode.Return))
+        {
+            FindObjectOfType<AudioManager>().Play("right");
+            clear();
+            score++;
+            scoreText.text = score.ToString();
+            randomIndex = Random.Range(0, bg.Length);
+            wordOutput.text = en[randomIndex];
+            getAnswer();
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            FindObjectOfType<AudioManager>().Play("wrong");
+            score = 0;
+            scoreText.text = score.ToString();
+            clear();
+            getAnswer();
+        }
+    }
+
+    private void clear()
+    {
+        inputfieldname.Select();
+        inputfieldname.text = "";
+    }
+
+    public void getAnswer()
+    {
+        var input = gameObject.GetComponent<InputField>();
+        var se = new InputField.SubmitEvent();
+        se.AddListener(SubmitAnswer);
+        input.onEndEdit = se;
+    }
+
+    private void SubmitAnswer(string arg0)
+    {
+        container = arg0;
+    }
+}
---
+++ Assets/scripts/WordGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class WordGame
+{
+    //key - english, value - bulgarian
+    public Dictionary<string, string> Words { get; set; }
+
+    public WordGame()
+    {
+        Words = new Dictionary<string, string>();
+        Words.Add("Hello", "Здравей"); //0
+        Words.Add("Bye", "Довиждане"); //1
+        Words.Add("Apple", "Ябълка"); //2
+        Words.Add("Carrot", "Морков"); //3
+        //Add more words by using Words.Add(english word, bulgarian word)
+    }
+
+    public KeyValuePair<string, string> GetRandomWord()
+    {
+        int randomIndex = Random.Range(0, Words.Count);
+        return Words.ElementAt(randomIndex);
+    }
+
+}
